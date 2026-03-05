6c67a957aaf0a86f420ae0b3c4329e52c8261c70
SamWen0201 <jqwertyu899800@gmail.com>
2026-03-05 21:04:16 +0800

    initial commit

---
+++ chord-finder/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Chord Finder</h1>
+    <p>in the template tag</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style></style>
